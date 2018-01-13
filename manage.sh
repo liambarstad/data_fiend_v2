9a048aad3de19b2fd052c718ca5ecc7d9d29d1c1
Liam Barstad <liambarstad@gmail.com>
2018-01-13 15:33:50 -0700

    shell script working and and initial layout

--- manage.sh
+++ manage.sh
@@ -2,34 +2,73 @@
 
 main_menu()
 {
-  case $1 in
-    startup)
-      startup $argv[2]
-      ;;
-    test)
-      tests $argv[2]
-      ;;
-    *)
-      startup $argv[2] &
-      tests $argv[2]
-      ;;
-  esac
+  if [ ! -z $1 ]; then
+    case $1 in
+      startup)
+        startup $2
+        ;;
+      test)
+        tests $2
+        ;;
+      *)
+        echo "Invalid option ${1} selected"
+        exit 1
+        ;;
+    esac
+  else
+    tests
+    startup
+  fi
 }
 
 startup()
 {
-  case $1 in
-    cereberus)
-      python cerberus/main.py
-      ;;
-    daedalus)
-      python daedalus/main.py
-      ;;
-    *)
-      python cerberus/main.py
-      #python daedalus/main.py
-      ;;
-  esac
+  if [ ! -z $1 ]; then
+    case $1 in
+      cerberus) 
+        echo "booting up ${1}..."
+        python cerberus/main.py
+        ;;
+      daedalus)
+        echo "booting up ${1}..."
+        python daedalus/main.py
+        ;;
+      *)
+        echo "Invalid option ${1} selected"
+        exit 1
+        ;;
+    esac
+  else
+    echo "booting up..."
+    python cerberus/main.py &
+    python daedalus/main.py
+  fi
 }
 
-main_menu $argv[1]
+tests()
+{
+  if [ ! -z $1 ]; then
+    case $1 in
+      cerberus)
+        echo "testing ${1}..."
+        nosetests cerberus/tests
+        ;;
+      daedalus)
+        echo "testing ${1}..."
+        nosetests daedalus/tests
+        ;;
+      *)
+        echo "Invalid option ${1} selected"
+        exit 1
+        ;;
+    esac
+  else
+    echo "testing..."
+    nosetests cerberus/tests
+    nosetests daedalus/tests
+  fi
+}
+
+ARG0=$1
+ARG1=$2
+main_menu $ARG0 $ARG1
